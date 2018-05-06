02dc8540796197dd0be97369461c4ea6903aa717
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-06 03:45:22 -0700

    add explicit returns

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'; //strict javascript rules, stronger security
 
 const gulp = require('gulp');
 const concat = require('gulp-concat');
@@ -7,45 +7,46 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const maps = require('gulp-sourcemaps');
 
-gulp.task("concatScripts", function(){//define a task with the name as 1st param
 
-	gulp.src([  //can take array or string of file names
+//define a task with the name as 1st param
+//can take array or string of file names, and load order matters for dependency (ie. jQuery needs to be before jQuery.sticky)
+gulp.task("concatScripts", function(){
+	return gulp.src([ //Explicitly return the stream it's creating. Works like a promise -> without the return statement, other tasks won't know when it finishes so they'll start right away without waiting for it to finish
 		"js/jquery.js",
-		"js/sticky/jquery.sticky.js", //depends on jquery
-		"js/main.js"]) //depends on jquery, so it needs to be loaded last
+		"js/sticky/jquery.sticky.js",
+		"js/main.js"])
+		.pipe(maps.init())
 		.pipe(concat("app.js"))//.pipe sends a readable stream of data to our concat task //piping lets you send things between programs ie: Node to JS(assembly)
+		.pipe(maps.write("./")) //put it in the directory relative to the one given below
 		.pipe(gulp.dest("js")); //gulp.dest method takes the readable string of data and persists it to disk
 });
 
 
 //minification speeds up load times, and it can sometimes rename variables for optimization
-gulp.task("minifyScripts", function(){
-	gulp.src("js/app.js")
+gulp.task("minifyScripts", ["concatScripts"], function(){ //adding concatScripts here makes it a dependency of minifyScripts and ensures it will run first
+	return gulp.src("js/app.js")
 		.pipe(uglify())
 		.pipe(rename("app.min.js")) //use gulp-rename to not overwrite app.js
 		.pipe(gulp.dest("js")); //gulp won't overwrite files when the file stream is output using gulp.dest
 });
 
 //compiling sass isn't hard, but it's time consuming to do every time you make a change. Gulp can automate this.
-//maps help define where the source of elements is in the project come from, so instead of just having 'css/application.css', it will break it down in dev tools to css/base/..., css/application.css, css/application.scss, etc."
+//SourceMaps -> maps help define where the source of elements is in the project come from, so instead of just having 'css/application.css', it will break it down in dev tools to css/base/..., css/application.css, css/application.scss, etc."
+//for JS it can show which file a bug comes from, making them easier to track down. (ie. compiled line bug: 1003 vs mapped bug: 25)
+//application.scss file is importing other scss (sass) files, which are themselves importing the actual source scss files. This is good modular practice.
 gulp.task('compileSass', function(){
-	//this file is importing other scss (sass) files, which are themselves importing the actual source scss files. This is good modular practice.
-	gulp.src("scss/application.scss")
+	return gulp.src("scss/application.scss")
 		.pipe(maps.init()) //gets piped to sass method, where the sass is actually compiled
 		.pipe(sass())
-		.pipe(maps.write("./")) //relative to our output directory, which is css (given below)
+		.pipe(maps.write("./")) //relative to our output directory, given below
 		.pipe(gulp.dest("css"));
 });
 
-
-
-
-
-//SourceMaps ->
+gulp.task("build", ['minifyScripts', 'compileSass']); //default is to run concurrently
 
 
 //smaller, module code is easier to maintain when you work on larger projects or larger teams
-//
-// gulp.task("default", ["hello"], function(){ //hello task is a dependency, so it will run before the default task, which is the body
-// 	console.log("Hello, Gulp! This is the Default Task!");
-// });
+
+gulp.task("default", ["hello"], function(){ //hello task is a dependency, so it will run before the default task, which is the body
+	console.log("Hello, Gulp! This is the Default Task!");
+});
