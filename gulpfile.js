661241776f2eee98c604cba60473590491e8b8a0
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-06 02:54:23 -0700

    update gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+
+//define a task with the name as 1st param
+gulp.task("concatScripts", function(){
+	gulp.src([  //can take array or string of file names
+		"js/jquery.js",
+		"js/sticky/jquery.sticky.js", //depends on jquery
+		"js/main.js"]) //depends on jquery, so it needs to be loaded last
+		.pipe(concat("app.js"))//.pipe sends a readable stream of data to our concat task //piping lets you send things between programs ie: Node to JS(assembly)
+		.pipe(gulp.dest("js")); //gulp.dest method takes the readable string of data and persists it to disk
+});
+
+gulp.task("default", ["hello"], function(){ //hello task is a dependency, so it will run before the default task, which is the body
+	console.log("This is the default task!");
+});
+
+//minification speeds up load times, and it can sometimes rename variables for optimization
